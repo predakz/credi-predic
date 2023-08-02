d47bdf951676b425b17d0302280b731e4c91aad2
Paul Russe <paul.russe@gmx.fr>
2023-08-02 17:31:06 +0200

    corected new unit test

--- test_main.py
+++ test_main.py
@@ -6,8 +6,10 @@ Created on Wed Aug  2 16:46:10 2023
 """
 
 import pytest
+import pandas as pd
 import main
 
 def test_check_data():
-    result = main.check_data(main.data)
+    data = pd.read_csv('cleaned_data.csv', index_col=0)
+    result = main.check_data(data)
     assert(result==1)
